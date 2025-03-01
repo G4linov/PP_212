b5d5df70ae1b7ea8234fa37c28da8026e0462737
Vyacheslavvvv <galin0ff@yandex.ru>
2025-03-01 19:47:13 +0300

    Ready PP212

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+
+        HelloWorld secondHelloWorldBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean == secondHelloWorldBean);
+
+        Cat catFirst = applicationContext.getBean("cat", Cat.class);
+        Cat catSecond = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(catFirst == catSecond);
     }
 }
